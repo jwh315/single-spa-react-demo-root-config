31653431e4d59b56b1e31745d80137a366cd3fa8
Jacob Holmes <jacob.holmes@coxautoinc.com>
2021-03-25 18:38:21 -0700

    build fixes

---
+++ build/after_deploy.sh
@@ -0,0 +1,18 @@
+set -e
+
+NEW_URL="https://single-spa-react-demo.s3.amazonaws.com/${PROJECT}/${GITHUB_SHA}/cd-${PROJECT}.js"
+
+echo "Downloading import map from S3"
+aws s3 cp s3://single-spa-react-demo/importmap.json importmap.json
+aws s3 cp s3://single-spa-react-demo/${PROJECT}/${GITHUB_SHA}/index.html index.html
+
+echo "Updating import map to point to new version of"
+echo $(jq --arg ARG1 "@cd/${PROJECT}" --arg ARG2 "${NEW_URL}" '.imports[$ARG1] = $ARG2' importmap.json) > importmap.json
+
+echo "New Url = ${NEW_URL}"
+cat importmap.json
+
+echo "Uploading new import map to S3"
+aws s3 cp importmap.json s3://single-spa-react-demo/importmap.json --cache-control 'public, must-revalidate, max-age=0' --acl 'public-read'
+aws s3 cp index.html s3://single-spa-react-demo/${PROJECT}/index.html --cache-control 'public, must-revalidate, max-age=0' --acl 'public-read'
+echo "Deployment successful"
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const singleSpaDefaults = require("webpack-config-single-spa");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = (webpackConfigEnv, argv) => {
-  const orgName = "react-mf";
+  const orgName = "cd";
   const defaultConfig = singleSpaDefaults({
     orgName,
     projectName: "root-config",
